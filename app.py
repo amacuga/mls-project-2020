fa91da47a317b9763d81bd8d7dcf491a967ea879
Alexandra Macuga <sasenah@gmail.com>
2021-01-08 22:20:44 +0100

    Updated app.py, index.html

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# Not finished, wasn't sure how to get model.predict() from jupyter to app.py and index.html
+
 # flask for web app.
 import flask as fl
 # numpy for numerical work.
@@ -11,12 +13,8 @@ app = fl.Flask(__name__)
 def home():
   return app.send_static_file('index.html')
 
-# Add uniform route.
-@app.route('/api/uniform')
-def uniform():
+# Add prediction.
+@app.route('/predict')
+def predict():
+  # needs to be modified, now it's just generating random value from uniform distibution
   return {"value": np.random.uniform()}
-
-# Add normal route.
-@app.route('/api/normal')
-def normal():
-  return {"value": np.random.normal()}
